02835a055573a379ae449360f179536116764795
Maciej Szarlinski <mszarlinski@gmail.com>
2017-01-08 17:18:20 +0100

    Initial project

---
+++ src/main/java/com/github/mszarlinski/hexagonal/HexagonalArchWorkshopApplication.java
@@ -0,0 +1,12 @@
+package com.github.mszarlinski.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchWorkshopApplication.class, args);
+	}
+}
